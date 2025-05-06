caeb1f89a6f52cd55ad1acdcf61e63dee8e0f970
Tdionhogh <159053190+Tdionhogh@users.noreply.github.com>
2025-05-06 23:14:30 +0200

    Nyt projekt med Gotorz.Client og opdateret løsning

---
+++ Gotorz/Gotorz.Client/Program.cs
@@ -0,0 +1,22 @@
+using Gotorz.Client;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace Gotorz.Client
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddCascadingAuthenticationState();
+            builder.Services.AddSingleton<AuthenticationStateProvider, PersistentAuthenticationStateProvider>();
+            // I Program.cs
+          
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
